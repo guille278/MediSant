ca98711d83dbf57b5a1b12c5ae23846f5089b808
guille278 <guillermoal278@gmail.com>
2022-10-08 19:52:19 -0500

    archivo config

---
+++ app/src/main/java/com/example/medisant/config/Config.java
@@ -0,0 +1,6 @@
+package com.example.medisant.config;
+
+public class Config {
+    private static final String HOST = "192.168.1.73";
+    public static final String API_URL = "http://" + HOST + "/proyecto-9no-web/medisant-admin/public/api";
+}
