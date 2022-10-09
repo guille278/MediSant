5fa778622b67631be81a74c5bfb569efaab20a0e
guille278 <guillermoal278@gmail.com>
2022-10-08 21:15:18 -0500

    binding request

--- app/src/main/java/com/example/medisant/adapters/ProductsAdapter.java
+++ app/src/main/java/com/example/medisant/adapters/ProductsAdapter.java
@@ -3,6 +3,7 @@ package com.example.medisant.adapters;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.medisant.R;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -25,8 +27,16 @@ public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.Produc
     }
 
     public class ProductViewHolder extends RecyclerView.ViewHolder {
+        ImageView productImage;
+        TextView productName, productShortDesc, productPrice, productAvailable;
+
         public ProductViewHolder(@NonNull View itemView) {
             super(itemView);
+            productImage = itemView.findViewById(R.id.iv_product_image);
+            productName = itemView.findViewById(R.id.tv_product_name);
+            productShortDesc = itemView.findViewById(R.id.tv_product_description);
+            productPrice = itemView.findViewById(R.id.tv_product_price);
+            productAvailable = itemView.findViewById(R.id.tv_product_available);
         }
     }
 
@@ -39,6 +49,14 @@ public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.Produc
 
     @Override
     public void onBindViewHolder(@NonNull ProductViewHolder holder, int position) {
+        try {
+            holder.productName.setText(productsList.getJSONObject(position).getString("name"));
+            holder.productShortDesc.setText(productsList.getJSONObject(position).getString("short_description"));
+            holder.productPrice.setText("$"+productsList.getJSONObject(position).getString("price"));
+            holder.productAvailable.setText("Disponibles: " + productsList.getJSONObject(position).getString("available"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
